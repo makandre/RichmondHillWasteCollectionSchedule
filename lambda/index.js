c558111ca0eec1ff8ab2919758c86a0dda3ca027
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-06-19 18:59:50 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -26,7 +26,7 @@ const getWeek = () => {
         date.setDate(date.getDate() + 1);
         
     const week = currentWeekNumber(date);
-    const colour = week % 2 === 0 ? 'blue' : 'yellow';
+    const colour = week % 2 !== 0 ? 'blue' : 'yellow';
     
     return { week, colour };
 };
